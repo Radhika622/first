d6678356466e6fe790f77fc0b3744d21f650122c
Radhika <radhikamca622@gmail.com>
2023-11-14 15:47:13 +0530

    first commit

---
+++ src/main/java/com/docker/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.docker.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController 
+{
+@RequestMapping("/")
+public String hello() 
+{
+return "Welcome its My first Hello world using docker file";
+}
+}
